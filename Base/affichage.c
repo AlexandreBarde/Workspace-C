4e545ad9582856507f26b9f071c5803020d4d81d
Krown0s <chronoos89@gmail.com>
2017-07-09 00:45:14 +0200

    Affichage avec la fonction write()

---
+++ Base/affichage.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+	write(1, &c, 1); //1 : La sortie de base du programme
+	return (0); 
+}
+
+int ft_nputchar(char c, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_putchar(c);
+		i = i + 1;
+	}
+	return 0;
+}
+
+int main ()
+{
+	ft_nputchar('@', 10);
+	ft_putchar('\n');
+	return (0);
+}
+
+
+
